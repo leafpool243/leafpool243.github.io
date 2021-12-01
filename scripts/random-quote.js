f3c73027ca7f32dcae9c5ec749d2f573676b9e0f
AmberWing <63614345+leafpool243@users.noreply.github.com>
2021-11-30 21:32:24 -0800

    Added random quote
    This random quote will appear on the homepage. There's currently not a
    lot of them because I can't think of anything more to add, but hopefully
    in the future, I'll add more.

---
+++ scripts/random-quote.js
@@ -0,0 +1,13 @@
+const quoteList = [
+    "Meow!! <3",
+    "Nyaa!! <3",
+    "Ooooh! hello there!!",
+    "uwu",
+    "owo",
+    "dogs bad /j",
+    "<q>We are NOT calling it <q>magical death spit</q>!!</q>"
+]
+const randomQuoteEl = document.getElementById("random-quote");
+const chosenQuote = quoteList[Math.floor(Math.random() * quoteList.length)]
+randomQuoteEl.innerHTML = chosenQuote;
+console.log(Math.random() * quoteList.length);
