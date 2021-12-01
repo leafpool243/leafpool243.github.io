98efcdf52c5a4c9094421d21b53eb28dd7cc609d
AmberWing <63614345+leafpool243@users.noreply.github.com>
2021-12-01 00:08:03 -0800

    Changed the thing for random quotes

--- scripts/random-quote.js
+++ scripts/random-quote.js
@@ -1,14 +1,15 @@
-const quoteList = [
-    "Meow!! <3",
-    "Nyaa!! <3",
-    "Ooooh! hello there!!",
-    "uwu",
-    "owo",
-    "dogs bad /j",
-    "<q>We are NOT calling it <q>magical death spit</q>!!</q>",
-    "Snudoo"
-]
-const randomQuoteEl = document.getElementById("random-quote");
-const chosenQuote = quoteList[Math.floor(Math.random() * quoteList.length)];
-randomQuoteEl.innerHTML = chosenQuote;
-console.log(Math.random() * quoteList.length);
+const randomQuote = () => {
+    const quoteList = [
+        "Meow!! <3",
+        "Nyaa!! <3",
+        "Ooooh! hello there!!",
+        "uwu",
+        "owo",
+        "dogs bad /j",
+        "<q>We are NOT calling it <q>magical death spit</q>!!</q>",
+        "Snudoo"
+    ]
+    const randomQuoteEl = document.getElementById("random-quote");
+    const chosenQuote = quoteList[Math.floor(Math.random() * quoteList.length)];
+    return chosenQuote;
+};
